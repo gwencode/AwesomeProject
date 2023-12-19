2225964a6c8db8fb3385a5af87b1de8c889a6f84
Gwendal Le Bris <gwen.coding@gmail.com>
2023-12-19 15:23:56 +0100

    Styling

--- App.tsx
+++ App.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
 const App: React.FC = () => {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={[styles.container, styles.pink]}>
         <Text>Hello World</Text>
+        {/* <Text style={styles.red}>Hello World</Text> */}
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  pink: {
+    backgroundColor: 'pink',
+  },
+  // red: {
+  //   backgroundColor: 'red',
+  // },
+  container: {
+    paddingVertical: 16,
+    paddingHorizontal: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  safeArea: {
+    flex: 1,
+  },
+});
 
 export default App;
