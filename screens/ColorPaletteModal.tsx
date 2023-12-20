12638a392d0736907f537360b21938a0783d4a10
Gwendal Le Bris <gwen.coding@gmail.com>
2023-12-20 14:34:39 +0100

    Set name of the color palette

--- screens/ColorPaletteModal.tsx
+++ screens/ColorPaletteModal.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+} from 'react-native';
 
 const ColorPaletteModal = () => {
+  const [name, setName] = useState<string>('');
+
   return (
-    <View>
-      <Text>Color Palette Modal</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Text>Name of your color scheme</Text>
+        <TextInput style={styles.input} value={name} onChangeText={setName} />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
-// const styles = StyleSheet.create({
-// });
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 16,
+    paddingHorizontal: 24,
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  input: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+});
 
 export default ColorPaletteModal;
